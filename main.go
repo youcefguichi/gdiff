e08fcc294a24c5a012d98d08a8f2078dc78acbf1
Youcef Guichi <youcef.guichi@adesso.eu>
2025-02-13 01:09:50 +0100

    comparing two files of 4,1 mb using my git takes me 18596 ms :( hiohohahah

--- main.go
+++ main.go
@@ -1,8 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
 	file1 := readFile("text1.txt")
 	file2 := readFile("text2.txt")
+	startTime := time.Now().UnixMilli()
 	diff, lineChangesTracker, removed, inserted := generateDiff(file1, file2)
 	PrintDiff(diff, file1, file2, removed, inserted, lineChangesTracker, 4)
+	endTime := time.Now().UnixMilli()
+	fmt.Printf("Execution time: %d ms\n", endTime-startTime)
 }
